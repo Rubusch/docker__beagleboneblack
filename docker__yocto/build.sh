d011c77dabf104bab4a3ffce87ad74a8a8714216
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-25 10:08:16 +0100

    build.sh: added

---
+++ docker__yocto/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+export BUILDDIR=~/poky/build
+chown $(whoami):$(whoami) -R $BUILDDIR
+
+## config files
+cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
+cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
+
+## source again, before start building
+cd ~/poky
+source oe-init-build-env $BUILDDIR
+
+## build
+bitbake core-image-minimal
