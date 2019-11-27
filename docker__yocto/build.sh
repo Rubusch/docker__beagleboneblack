964099ebe4c54c14062aea4504caeaccac84a28a
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-27 15:31:42 +0100

    build.sh: first a 'build/conf' folder must be around, to copy something into - fix

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -2,16 +2,17 @@
 export BUILDDIR=~/poky/build
 chown $(whoami):$(whoami) -R $BUILDDIR
 
+
 ## adjust config files
 sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample
-
-## layers.conf and bblayers.conf (edit those two!)
-cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
-cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
 
 ## source again, before start building
 cd ~/poky
 source oe-init-build-env $BUILDDIR
+
+## layers.conf and bblayers.conf (edit those two!)
+cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
+cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
 
 ## build
 bitbake core-image-minimal
