1e5113f89a713f73a64150617e4a3773e05ab5a4
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-27 16:32:39 +0100

    meta-lothars-configs: adjust configs, fixup

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -2,10 +2,6 @@
 export BUILDDIR=~/poky/build
 chown $(whoami):$(whoami) -R $BUILDDIR
 
-
-## adjust config files
-sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample
-
 ## source again, before start building
 cd ~/poky
 source oe-init-build-env $BUILDDIR
@@ -13,6 +9,9 @@ source oe-init-build-env $BUILDDIR
 ## layers.conf and bblayers.conf (edit those two!)
 cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
 cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
+
+## adjust config files
+sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i ${BUILDDIR}/conf/bblayers.conf
 
 ## build
 bitbake core-image-minimal
