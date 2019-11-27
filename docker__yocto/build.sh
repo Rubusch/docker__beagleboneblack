ed6c7330854f37c66f96003b5000697834d55cb7
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-27 11:09:42 +0100

    meta-lothars-configs: adjust paths to keep meta layer inside mounted link

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -2,10 +2,10 @@
 export BUILDDIR=~/poky/build
 chown $(whoami):$(whoami) -R $BUILDDIR
 
-## config files
-sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i /home/$USER/poky/meta-lothars-configs/conf/bblayers.conf.sample
-cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
-cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
+## adjust config files
+sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i /home/$USER/poky/build/meta-lothars-configs/conf/bblayers.conf.sample
+cp -arf /home/$(whoami)/poky/build/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
+cp -arf /home/$(whoami)/poky/build/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
 
 ## source again, before start building
 cd ~/poky
