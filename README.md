e911697860eeddeef3b04d15c51399d057300ffd
Lothar Rubusch <l.rubusch@gmx.ch>
2019-11-27 15:26:52 +0100

    meta-lothars-configs: change (again) how to provide the costumized meta layer

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ $ docker images
     rubuschl/bbb-yocto 20191104161353      cbf4cb380168        24 minutes ago      10.5GB
     ubuntu             xenial              5f2bf26e3524        4 days ago          123MB
 
-$ time docker run -ti -v $PWD/output:/home/$USER/poky/build --user=$USER:$USER --workdir=/home/$USER rubuschl/bbb-yocto:20191104161353
+$ time docker run -ti -v $PWD/output:/home/$USER/poky/build -v $PWD/meta-lothars-configs:/home/$USER/poky/meta-lothars-configs --user=$USER:$USER --workdir=/home/$USER rubuschl/bbb-yocto:20191104161353
 ```
 
 For debugging / working in the container, append ``/bin/bash`` to the above command.
--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -3,9 +3,11 @@ export BUILDDIR=~/poky/build
 chown $(whoami):$(whoami) -R $BUILDDIR
 
 ## adjust config files
-sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i /home/$USER/poky/build/meta-lothars-configs/conf/bblayers.conf.sample
-cp -arf /home/$(whoami)/poky/build/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
-cp -arf /home/$(whoami)/poky/build/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
+sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample
+
+## layers.conf and bblayers.conf (edit those two!)
+cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
+cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
 
 ## source again, before start building
 cd ~/poky
