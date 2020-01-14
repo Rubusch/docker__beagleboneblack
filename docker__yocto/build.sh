238e31337cec8c04a1c030bb3c1bd6e651533568
Lothar Rubusch <l.rubusch@gmx.ch>
2020-01-14 13:07:47 +0100

    build.sh: update

--- docker__yocto/build.sh
+++ docker__yocto/build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
-export BUILDDIR=~/poky/build
+export YOCTODIR="/home/$(whoami)/poky"
+export BUILDDIR="${YOCTODIR}/build"
 chown $(whoami):$(whoami) -R $BUILDDIR
 
 ## source again, before start building
-cd ~/poky
+cd ${YOCTODIR}
 source oe-init-build-env $BUILDDIR
 
 ## layers.conf and bblayers.conf (edit those two!)
-cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
-cp -arf /home/$(whoami)/poky/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
+cp -arf ${YOCTODIR}/meta-lothars-configs/conf/bblayers.conf.sample ${BUILDDIR}/conf/bblayers.conf
+cp -arf ${YOCTODIR}/meta-lothars-configs/conf/local.conf.sample ${BUILDDIR}/conf/local.conf
 
 ## adjust config files
 sed "s/  ~\/poky\/meta/  \/home\/$(whoami)\/poky\/meta/g" -i ${BUILDDIR}/conf/bblayers.conf
