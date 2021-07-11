b858316afe682fc8dd363cec58717fd10996b46c
Lothar Rubusch <l.rubusch@gmail.com>
2021-07-11 12:35:49 +0200

    build.sh: update to regular buildroot

--- docker__buildroot/build_context/build.sh
+++ docker__buildroot/build_context/build.sh
@@ -13,9 +13,9 @@ if [ -z "${FIRST}" ]; then
     cd "${BR_DIR}"
     ## get (my) sources
     ## NB: the official buildroot is not able to build cross-mounted devices (thus using my patched buildroot tree)
-    git clone -j4 --depth=1 --branch "lothar/${BRANCH}" https://github.com/Rubusch/buildroot.git
+    #git clone -j4 --depth=1 --branch "lothar/${BRANCH}" https://github.com/Rubusch/buildroot.git
     ## oficial buildroot (won't work due to Kbuild's use of "rename()" for moving files which is not able to move cross-device (mounted folder)
-    #RUN cd /home/$USER && git clone -j4 --depth=1 --branch ${BRANCH} https://github.com/buildroot/buildroot.git buildroot
+    git clone -j4 --depth=1 --branch ${BRANCH} https://github.com/buildroot/buildroot.git
 fi
 
 cd "${BR_DIR}"
