74dd62d5c5741957af04428bf96c464fc622e926
Lothar Rubusch <l.rubusch@gmail.com>
2021-07-11 12:14:47 +0200

    build.sh: cleanup, conditional clone, shellcheck
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- docker__buildroot/build_context/build.sh
+++ docker__buildroot/build_context/build.sh
@@ -1,20 +1,26 @@
-#!/bin/bash -e
+#!/bin/sh -e
 
-export USER="$(whoami)"
-export DEFCONFIG=beaglebone_defconfig
-export MY_HOME=/home/${USER}
+MY_USER="$(whoami)"
+DEFCONFIG="beaglebone_defconfig"
+MY_HOME="/home/${MY_USER}"
+BR_DIR="${MY_HOME}/buildroot"
+BRANCH="2020.11.x"
 
-sudo chown "${USER}:${USER}" -R "${MY_HOME}/buildroot"
+sudo chown "${MY_USER}:${MY_USER}" -R "${BR_DIR}"
 
-## get (my) sources
-## NB: the official buildroot is not able to build cross-mounted devices (thus using my patched buildroot tree)
-git clone -j4 --depth=1 --branch "lothar/${BRANCH}" https://github.com/Rubusch/buildroot.git buildroot
-## oficial buildroot (won't work due to Kbuild's use of "rename()" for moving files which is not able to move cross-device (mounted folder)
-#RUN cd /home/$USER && git clone -j4 --depth=1 --branch ${BRANCH} https://github.com/buildroot/buildroot.git buildroot
+FIRST="$(ls -A "${BR_DIR}")"
+if [ -z "${FIRST}" ]; then
+    cd "${BR_DIR}"
+    ## get (my) sources
+    ## NB: the official buildroot is not able to build cross-mounted devices (thus using my patched buildroot tree)
+    git clone -j4 --depth=1 --branch "lothar/${BRANCH}" https://github.com/Rubusch/buildroot.git
+    ## oficial buildroot (won't work due to Kbuild's use of "rename()" for moving files which is not able to move cross-device (mounted folder)
+    #RUN cd /home/$USER && git clone -j4 --depth=1 --branch ${BRANCH} https://github.com/buildroot/buildroot.git buildroot
+fi
 
-cd "${MY_HOME}/buildroot"
+cd "${BR_DIR}"
 make defconfig "${DEFCONFIG}"
-make -j8
+make -j"$(nproc)"
 
 echo "READY."
 echo
